d2af31443275b17735498dddfae0557bd70c14d1
Florian <florian.huteau@epsi.fr>
2023-02-11 19:01:01 +0100

    Controller

---
+++ src/main/java/com/example/mspr/Controller/PlanteController.java
@@ -0,0 +1,9 @@
+package com.example.mspr.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/plante")
+public class PlanteController {
+}
